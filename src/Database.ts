8d6392b679a4f46f1457f75c58b69db2e72c08d7
andrey.ovcharenko@branderstudio.com <andrey.ovcharenko@branderstudio.com>
2020-02-24 23:16:00 +0200

    add Database class

---
+++ src/Database.ts
@@ -0,0 +1,21 @@
+import * as nano from 'nano';
+
+
+export class Database {
+  private conn: any
+  private db: any
+  constructor({ host, port, user, password }: {host: string, port: number, user: string, password: string}) {
+    const url = `http://${user}:${password}@${host}:${port}`;
+    this.conn = nano(url);
+  }
+  async create(name: string) {
+    const created = await this.conn.db.create(name);
+    console.log(created)
+  }
+  use(name: string) {
+    return this.conn.use(name);
+  }
+  insert(...params: any[]) {
+    return this.db.insert(...params);
+  }
+}
--- src/__tests__/greeter.test.ts
+++ src/__tests__/greeter.test.ts
@@ -1,5 +1,19 @@
 import { greeter } from '../index';
+import {Database} from '../Database'
+
+const conn = {
+  host: '127.0.0.1',
+  port: 5984,
+  user: 'admin',
+  password: 'password'
+}
+
+const db = new Database(conn)
 
 test('My Greeter', () => {
   expect(greeter('Carl')).toBe('Hello Carl');
+});
+
+test('create database', () => {
+  return  db.use('test').insert({a: 11});
 });
