cfdb2b009f57e967c8297df0c679f0c2b10ad76b
Андрей Овчаренко <ovcharenkoav@meta.ua>
2020-03-11 12:25:47 +0200

    checnge linter

--- src/Model.ts
+++ src/Model.ts
@@ -1,30 +1,18 @@
 import 'reflect-metadata';
-import {enumerable} from './decorators'
+import { enumerable } from './decorators';
 
 export class Model<IType> {
-
-    private __data__: IType
+    protected __DATA__: IType;
 
     constructor(data: IType) {
-        this.__data__ = data;
-    }
-
-    @enumerable(false)
-    getData(): IType {
-        return this.__data__;
-    }
-
-    @enumerable(false)
-    setData(data: IType) {
-        this.__data__ = data;
+        this.__DATA__ = data;
     }
 
     public toJSON(): any {
         const meta = 'nacho:property';
         const proto = Object.getPrototypeOf(this);
         const jsonObj: any = {};
-        for (let name in this) {
-            console.log(name)
+        for (const name in this) {
             if (Reflect.getMetadata(meta, proto, name)) {
                 if (typeof (this as any)[name] === 'object') {
                     jsonObj[name] = (this as any)[name].toJSON();
@@ -32,7 +20,17 @@ export class Model<IType> {
                     jsonObj[name] = (this as any)[name];
                 }
             }
-        };
+        }
         return jsonObj;
+    }
+
+    @enumerable(false)
+    protected getData(): IType {
+        return this.__DATA__;
+    }
+
+    @enumerable(false)
+    protected setData(data: IType) {
+        this.__DATA__ = data;
     }
 }
