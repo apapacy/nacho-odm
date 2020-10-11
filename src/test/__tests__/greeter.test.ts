5269e8932b02e2458bff2ffd8d68ea69f1bb6089
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2020-10-11 23:37:34 +0300

    toJSON() for array

--- src/test/__tests__/greeter.test.ts
+++ src/test/__tests__/greeter.test.ts
@@ -6,7 +6,7 @@ const db = new Reposytory();
 
 test('create Author', async () => {
   const author = new Author(<Author>{
-    _key: "1234567890",
+    //_key: "1234567890",
     name: 'Joe',
     address: <Address>{
       _key: '123',
@@ -32,10 +32,12 @@ test('create Author', async () => {
     }]
   })
   //author.name = 12
-  console.log(author)
-  console.log(author.toJSON())
-  //const doc: Author =  await db.authorCreate(author);
-  //console.log(doc);
+  //console.log(author)
+  //console.log(author.toJSON())
+  const doc: Author =  await db.authorCreate(author);
+  console.log(doc);
+  console.log(doc.toJSON());
+  console.log(doc.get(['cat', 'dog'], 'uk'));
   //const arr: Array<Author> =  await db.authorFindAll();
   //console.log('222222222222222222222', arr)
   //console.log('222222222222222222222', doc.get(['cat', 'dog']))
