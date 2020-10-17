16c041f1a180e9286b65573af59dcc00c7c5e769
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2020-10-17 23:25:31 +0300

    disable linter on _type

--- src/decorators/_type.ts
+++ src/decorators/_type.ts
@@ -1,3 +1,4 @@
+/* tslint:disable */
 export function _type(_type: string) {
     return function <T extends { new(...args: any[]): {} }>(constructor: T) {
         return class extends constructor {
