6fe88ecc565f081310bd54dcf800096567698923
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2020-10-18 01:48:22 +0300

    fix translatable type

--- lib/decorators/translatable.d.ts
+++ lib/decorators/translatable.d.ts
@@ -1,1 +1,1 @@
-export declare function translatable(): (target: any, propertyKey: string | symbol, descriptor?: PropertyDescriptor | undefined) => void;
+export declare function translatable(Type?: new (...args: any[]) => void): (target: any, propertyKey: string | symbol, descriptor?: PropertyDescriptor | undefined) => void;
--- src/decorators/translatable.ts
+++ src/decorators/translatable.ts
@@ -1,9 +1,9 @@
 import { setDescriptor } from './descriptor';
 import { Translatable } from '../translatable';
 
-export function translatable() {
+export function translatable(Type?:  new(...args: any[]) => void) {
   return (target: any, propertyKey: string | symbol, descriptor?: PropertyDescriptor): void => {
       setDescriptor(target, propertyKey, 'translatable', true);
-      setDescriptor(target, propertyKey, 'type', Translatable);
+      setDescriptor(target, propertyKey, 'type', Type || Translatable);
     }
 }
