832fc24e72bf036b21d8c339713b3d80a6905658
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2020-12-20 15:25:13 +0200

    rename variables

--- src/model.ts
+++ src/model.ts
@@ -13,26 +13,26 @@ export class Model<Type extends ModelType> implements ModelType {
     @persistent()
     @optional()
     @group('_all')
-    public _type: string | undefined;
+    public _type?: string;
 
     @persistent()
     @optional()
     @group('_all')
-    public _key: string | undefined;
+    public _key?: string;
 
     @persistent()
     @optional()
     @group('_all')
-    public _id: string | undefined;
+    public _id?: string;
 
     @persistent()
     @optional()
     @group('_all')
-    public _rev: string | undefined;
+    public _rev?: string;
 
     constructor(data: any) {
-        const proto = Object.getPrototypeOf(this);
-        const descriptors = getDescriptors(proto);
+        const prototype = Object.getPrototypeOf(this);
+        const descriptors = getDescriptors(prototype);
         for (const name of Object.keys(descriptors)) {
             const descriptor = descriptors[name] as Descriptor;
             if (descriptor.required && descriptor.attr && !descriptor.array && typeof data[name] === 'undefined') {
@@ -65,62 +65,62 @@ export class Model<Type extends ModelType> implements ModelType {
     public toJSON(): any {
         const proto = Object.getPrototypeOf(this);
         const descriptors = getDescriptors(proto);
-        const jsonObj: any = {};
+        const jsonObject: any = {};
         for (const name of Object.keys(descriptors)) {
             const descriptor = descriptors[name] as Descriptor;
             if (descriptor.attr && descriptor.array) {
-                jsonObj[name] = [];
+                jsonObject[name] = [];
                 (this as any)[name].forEach((item: any) => {
                     if (typeof (item as any)[name] === 'object') {
-                        jsonObj[name].push(item.toJSON());
+                        jsonObject[name].push(item.toJSON());
                     } else {
-                        jsonObj[name].push(item);
+                        jsonObject[name].push(item);
                     }
                 });
             } else if (descriptor.attr) {
                 if (typeof (this as any)[name] === 'object') {
-                    jsonObj[name] = (this as any)[name].toJSON();
+                    jsonObject[name] = (this as any)[name].toJSON();
                 } else {
-                    jsonObj[name] = (this as any)[name];
+                    jsonObject[name] = (this as any)[name];
                 }
             }
         }
-        return jsonObj;
+        return jsonObject;
     }
 
     public get(groups: string[], locale?: string): Type {
-        const proto = Object.getPrototypeOf(this);
-        const descriptors = getDescriptors(proto);
-        const jsonObj: any = {};
+        const prototype = Object.getPrototypeOf(this);
+        const descriptors = getDescriptors(prototype);
+        const jsonObject: any = {};
         for (const name of Object.keys(descriptors)) {
             const descriptor = descriptors[name] as Descriptor;
             if (descriptor?.groups?.[0] === '_all' || descriptor?.groups?.some((item) => groups?.indexOf(item) > -1)) {
                 if (descriptor?.array) {
                     if (descriptor.constr) {
-                        jsonObj[name] = [];
+                        jsonObject[name] = [];
                         for (const item of (this as any)[name]) {
-                            jsonObj[name].push(item.get(groups, locale));
+                            jsonObject[name].push(item.get(groups, locale));
                         }
                     } else {
-                        jsonObj[name] = [...(this as any)[name]];
+                        jsonObject[name] = [...(this as any)[name]];
                     }
                     continue;
                 }
                 if (descriptor?.constr) {
                     if (descriptor.translatable) {
                         if (locale) {
-                            jsonObj[name] = (this as any)[name][locale];
+                            jsonObject[name] = (this as any)[name][locale];
                         } else {
-                            jsonObj[name] = Object.assign((this as any)[name]);
+                            jsonObject[name] = Object.assign((this as any)[name]);
                         }
                     } else {
-                        jsonObj[name] = (this as any)[name].get(groups, locale);
+                        jsonObject[name] = (this as any)[name].get(groups, locale);
                     }
                 } else {
-                    jsonObj[name] = (this as any)[name];
+                    jsonObject[name] = (this as any)[name];
                 }
             }
         }
-        return jsonObj;
+        return jsonObject;
     }
 }
